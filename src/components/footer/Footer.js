8788ecd6e6ae0b41911b9504d89a12dceb181b71
DILEESHA1 <dileeshanawarathna18@gmail.com>
2022-02-11 00:20:54 +0530

    first commit

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './footer.css'
+const Footer = () => {
+  return (
+   <>
+   <div className="footer">
+   Designed by © 2022 All rights reserved🤍
+   </div>
+   </>
+  )
+}
+
+export default Footer
---
+++ src/pages/Home/Home.js
@@ -0,0 +1,58 @@
+import React from "react";
+import "./home.css";
+import { VscSettingsGear } from "react-icons/vsc";
+import { GiChart } from "react-icons/gi";
+import { IoMagnetSharp } from "react-icons/io5";
+import { MdReportProblem } from "react-icons/md";
+const Home = () => {
+  return (
+    <>
+      <div className="home">
+        <div className="homer">
+          <h1>
+         We grow brands,create experience and solve business problems.
+          </h1>
+          <div className="headeropt">
+          <button className="header-btn">Join Us</button>
+          </div>
+          <ul className="hul">
+            <div className="changer">
+              <li>
+                <VscSettingsGear style={{ color: "white",margin:"0px 20px"  }} />
+                <div className="title">
+                Business Planing<br/>joinus now
+                </div>
+              </li>
+            </div>
+            <div className="changer">
+              <li>
+                <GiChart style={{ color: "white",margin:"0px 20px" }} />
+                <div className="title">
+                   Business Analysis<br/>joinus now
+                </div>
+              </li>
+            </div>
+            <div className="changer">
+              <li>
+                <IoMagnetSharp style={{ color: "white",margin:"0px 20px" }} />
+                <div className="title">
+                 Brand Developing<br/>joinus now
+                </div>
+              </li>
+            </div>
+            <div className="changer">
+              <li>
+                <MdReportProblem style={{ color: "white",margin:"0px 20px" }} />
+                <div className="title">
+                    Problem Solving<br/>joinus now
+                </div>
+              </li>
+            </div>
+          </ul>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
