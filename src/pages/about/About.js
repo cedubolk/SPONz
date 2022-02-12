f3ef331eade505eba4b5bdb15624172e3a233b65
DILEESHA1 <dileeshanawarathna18@gmail.com>
2022-02-12 18:17:30 +0530

    added

---
+++ src/pages/about/About.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./about.css";
+
+const About = () => {
+  return (
+    <div className="mainabout">
+      <div className="abr"></div>
+      <div className="about-details">
+        <span className="about-dev">About</span>
+        <p className="about-par">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Est ducimus
+          aliquid ratione adipisci porro ex, explicabo, modi iusto obcaecati
+          officiis magni fuga, ipsam beatae ea? Odio assumenda voluptates
+          provident laborum? Praesentium, a! Doloribus, quidem excepturi,
+          corrupti sunt architecto atque ex officiis amet nam tempore est magni
+          ipsa ad, nihil rerum beatae suscipit dolorum nobis alias ducimus?
+          Expedita similique nesciunt libero!
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default About;
