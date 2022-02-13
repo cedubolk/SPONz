40daf6efb026a6d67023e55d372db8677aa171d4
lnathilina <thilina.aravinda1999@gmail.com>
2022-02-13 12:35:00 +0530

    services sample1

---
+++ src/pages/services/sem/Sem.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import "./sem.css";
+import {GiSemiClosedEye} from "react-icons/gi";
+const Sem = () => {
+  return (
+ <div className="Sem">
+ <h2>Search Engine Marketing</h2>
+    <p className="sem1">While SEM and SEO both rely on keywords that is where the similarity ends. SEM strategies involve paying a search engine to place your website in front of customers. It is closely related to pay-per-click advertising.</p>
+    <li className="sem2" >
+    <GiSemiClosedEye style={{ color: "black", fontsize: "500px" }}/>
+     </li>
+    </div>
+  )
+}
+
+export default Sem
