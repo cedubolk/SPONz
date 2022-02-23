e7f908b0158b97ef8c57bff5f9ba5758dd1a5075
DILEESHA1 <dileeshanawarathna18@gmail.com>
2022-02-24 00:23:33 +0530

    history pushes

--- src/pages/Home/Expectation.js
+++ src/pages/Home/Expectation.js
@@ -1,20 +1,37 @@
 import React from "react";
-import './expectation.css'
+import "./expectation.css";
+import { useHistory } from "react-router-dom";
 
 const Expectation = () => {
+
+let history = useHistory();
+
   return (
     <div className="main-exp">
       <div className="expectleft">
-<h2 className="expectmain">What to expect from us.</h2>
-<div className="allparexpect">
-<p className="expectpar">Responsive website designs to understand your<br/> customers’ behavior across key platforms.</p>
-<p className="expectpar">Customer-centric User Experience design to ensure your <br/>customers seamlessly navigate through their goals.</p>
-<p className="expectpar">Digital Innovation Consulting to help you build strong <br/>customer relationships and make insightful decisions.</p>
-</div>
-<button className="expectbtn">View more</button>
+        <h2 className="expectmain">What to expect from us.</h2>
+        <div className="allparexpect">
+          <p className="expectpar">
+            Responsive website designs to understand your
+            <br /> customers’ behavior across key platforms.
+          </p>
+          <p className="expectpar">
+            Customer-centric User Experience design to ensure your <br />
+            customers seamlessly navigate through their goals.
+          </p>
+          <p className="expectpar">
+            Digital Innovation Consulting to help you build strong <br />
+            customer relationships and make insightful decisions.
+          </p>
+        </div>
+        <button onClick={()=>{history.push("/about")}} className="expectbtn">View more</button>
       </div>
       <div className="expectright">
-<img className="landingimger" src="https://vdepict.com/wp-content/uploads/2020/06/2.HM_.Image2MIN.png" alt="" />
+        <img
+          className="landingimger"
+          src="https://vdepict.com/wp-content/uploads/2020/06/2.HM_.Image2MIN.png"
+          alt=""
+        />
       </div>
     </div>
   );
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,29 +1,31 @@
-import React from "react";
+import { useHistory } from "react-router-dom";
 import "./home.css";
 import { VscSettingsGear } from "react-icons/vsc";
 import { GiChart } from "react-icons/gi";
 import { IoMagnetSharp } from "react-icons/io5";
 import { MdReportProblem } from "react-icons/md";
-import header from '../../assets/assests/header.png'
+import header from "../../assets/assests/header.png";
 const Home = () => {
+
+let history = useHistory();
+
   return (
-    
-      <div className="home">
-       <div className="homeleft">
-         <h1 className="landingmain">Crafting Memorable Digital Experiences</h1>
-         <p className="landingpar">We build state-of-the-art web apps and validated digital product designs to help you grow.</p>
-         <button className="landingbtn">get started</button>
-       </div>
-       <div className="homeright">
-         <div className="landingimg">
-           <img className="landingimg" src={header} alt="" />
-         </div>
-       </div>
+    <div className="home">
+      <div className="homeleft">
+        <h1 className="landingmain">Crafting Memorable Digital Experiences</h1>
+        <p className="landingpar">
+          We build state-of-the-art web apps and validated digital product
+          designs to help you grow.
+        </p>
+        <button onClick={()=>{history.push("/contact")}} className="landingbtn">get started</button>
       </div>
-    
+      <div className="homeright">
+        <div className="landingimg">
+          <img className="landingimg" src={header} alt="" />
+        </div>
+      </div>
+    </div>
   );
 };
 
 export default Home;
-
-
